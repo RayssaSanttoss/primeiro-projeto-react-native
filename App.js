7b0d72540c31faab5247e6c0476ef000ce3c6fe8
RayssaSanttoss <43547070+RayssaSanttoss@users.noreply.github.com>
2020-09-17 18:39:08 -0300

    Primeiro Commit

---
+++ App.js
@@ -0,0 +1,48 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default class App extends React.Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+      texto: "Texto Inicial",
+      contador: 0
+    };
+  }
+  render () {
+    return (
+      <View style={styles.container}>
+        <Text>{this.state.contador}</Text>
+        <Button
+          title="Incrementar"
+          onPress={() => this.setState({contador: this.state.contador + 1})}
+        />
+      </View>
+    );
+  }
+}
+
+/*export default function App() {
+
+  const[texto, setTexto] = useState("Texto inicial");
+
+  return (
+    <View style={styles.container}>
+      <Text>{texto}</Text>
+      <Button title="Alterar texto"
+      onPress={() => {setTexto("Novo texto")}}
+        />
+      <StatusBar style="auto" />
+    </View>
+  );
+}*/
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
